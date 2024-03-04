ce2958a47a1b3ec4cc9595c4e84860a47fb1ad84
hinet <63603636@qq.com>
2024-03-04 18:16:37 +0800

    Update i18n.go

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -17,21 +17,21 @@ var (
 	translator        *Translator
 )
 
-// MessageMap 存储不同语言的翻译信息
+// MessageMap Store translation information for different languages
 type MessageMap map[string]interface{}
 
 func init() {
 	flag.StringVar(&localLanguageFile, "lang", "config/lang/", "init language config")
 }
 
-// Translator 提供翻译功能
+// Translator translation provide
 type Translator struct {
 	translations map[language.Tag]MessageMap
 	mu           sync.RWMutex
 	language     language.Tag
 }
 
-// NewTranslator 创建新的翻译器
+// NewTranslator Create a new translator
 func NewTranslator() *Translator {
 	translator = &Translator{
 		translations: make(map[language.Tag]MessageMap),
@@ -46,7 +46,7 @@ func NewTranslator() *Translator {
 	return translator
 }
 
-// LoadLanguage 加载指定路径下的 JSON 语言包
+// LoadLanguage Load the JSON language pack under the specified path
 func (t *Translator) LoadLanguage() error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
@@ -85,53 +85,49 @@ func (t *Translator) SetLanguage(languageTag language.Tag) {
 	t.language = languageTag
 }
 
-// Translate 根据语言标签翻译给定的键，并进行变量替换
+// Translate the given key based on language tags and replace variables
 func (t *Translator) Translate(key string, params map[string]string) string {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	if messages, ok := t.translations[t.language]; ok {
-		return t.recursively(key, messages, params)
+		return t.recursive(key, messages, params)
 	}
-	return key // 默认返回键本身
+	return key // Default return key itself
 }
 
-// 递归转换翻译内容
-func (t *Translator) recursively(key string, messages MessageMap, params map[string]string) string {
-	// 如果已经到达最后一级键，返回对应的翻译结果
+// Recursive conversion of translated content
+func (t *Translator) recursive(key string, messages MessageMap, params map[string]string) string {
 	keys := strings.Split(key, ".")
 	if len(keys) == 0 {
 		return ""
 	}
-	// 获取当前级别的键
 	currentKey := keys[0]
-	// 获取当前级别的值
 	translation, ok := messages[currentKey]
 	if !ok {
-		return currentKey // 如果没有找到对应的键，返回原始键
+		return currentKey
 	}
-	// 如果是字符串，进行变量替换并返回
+	// If it is a string, replace the variable and return
 	if str, ok := translation.(string); ok {
-		// 如果已经是最后一级键，进行变量替换并返回
 		if len(keys) == 1 {
 			return interpolate(str, params)
 		}
 	}
-	// 如果是嵌套的 map，则继续递归翻译
+	// If it is a nested map, continue recursive translation
 	if nestedMap, ok := translation.(map[string]interface{}); ok {
 		k := strings.Join(keys[1:], ".")
-		return t.recursively(k, nestedMap, params)
+		return t.recursive(k, nestedMap, params)
 	}
-	return currentKey // 如果值不是字符串也不是嵌套 map，则返回原始键
+	return currentKey // If the value is not a string or a nested map, return the original key
 }
 
-// interpolate 替换翻译字符串中的变量
+// interpolate Replace variables in translation strings
 func interpolate(translation string, params map[string]string) string {
 	for key, value := range params {
 		translation = strings.ReplaceAll(translation, "{"+key+"}", value)
 	}
 	return translation
 }
-
+//GetTranslator 
 func GetTranslator() *Translator {
 	if translator == nil {
 		translator = NewTranslator()
