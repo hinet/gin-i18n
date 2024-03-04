c82124a60d7e9cc424815e13bdab558b0adbc2d1
hinet <63603636@qq.com>
2024-03-04 18:17:47 +0800

    Update interceptor.go

--- i18n/interceptor.go
+++ i18n/interceptor.go
@@ -13,16 +13,15 @@ func LanguageInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryS
 		return nil, fmt.Errorf("failed to get metadata from context")
 	}
 
-	// 从元数据中提取HTTP标头中的"accept-language"信息
+	// Extracting "Accept-Language" information from HTTP headers from metadata
 	acceptLanguages := md.Get("accept-language")
 	if len(acceptLanguages) == 0 {
 		return nil, fmt.Errorf("accept-language header not found")
 	}
 	language := acceptLanguages[0]
-	//初始化语言转换器
+	//Initialize translator
 	translator := NewTranslator()
 	tag := GetPreferredLanguage(language)
 	translator.SetLanguage(tag)
-	// 调用下一个处理程序
 	return handler(ctx, req)
 }
